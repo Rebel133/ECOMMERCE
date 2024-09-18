cb09abf9f54be76bbc2687f4b67c6a9109a5ebdc
Adarsh Maskar <adarsh.maskar@cheetahdigital.com>
2024-09-19 01:29:22 +0530

    Added Header and Footer Section in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
-  return;
-  <>
-    <Outlet />
-  </>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
