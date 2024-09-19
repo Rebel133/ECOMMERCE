628a0a22ac9907df5d40badb04b88dd243c99382
Adarsh Maskar <adarsh.maskar@cheetahdigital.com>
2024-09-19 18:47:43 +0530

    Created ForgotPassword.jsx page and new route for the ForgotPassword

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../Pages/Home";
+import Login from "../Pages/Login";
+import ForgotPassword from "../Pages/ForgotPassword";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +12,14 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "forgot-password",
+        element: <ForgotPassword />,
       },
     ],
   },
